31bd7b5ecd00d1045171026f2833cd6d3c4ab44d
John Atkins <johnatkins1999@yahoo.com>
2017-08-07 12:20:54 -1000

    commit

---
+++ public/js/myApp.js
@@ -0,0 +1,28 @@
+// creation uses a 2nd array argument to import dependencies
+angular.module('myApp', []);
+
+// retrieval has only one argument
+var myApp = angular.module('myApp');
+
+myApp
+.config(function(){
+    // config
+})
+.run(function(){
+    // initialize
+    console.log("running");
+});
+
+// // Controller
+// // Controller
+// // Code Samples
+// var myApp = angular.module('myApp');
+
+// myApp.controller('MyController', function(){
+
+// });
+
+// myApp.controller('MyController', ['$scope', function($scope){
+
+// });
+// $scope
