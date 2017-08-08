94d81d9bd08630f63009e6b627ee81ed27037640
John Atkins <johnatkins1999@yahoo.com>
2017-08-07 14:45:59 -1000

    xxx

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,8 +1,10 @@
 /*jshint esversion: 6*/
 var myApp = angular.module('myApp');
 
-myApp.controller('MyController',  ['$scope', 'mainCharacter', function($scope, mainCharacter) {
+myApp.controller('MyController',  ['$scope', 'CharacterVersionFactory', 'mainCharacter',
+  function($scope, CharacterVersionFactory, mainCharacter) {
   $scope.myFirstName ='John';
   $scope.myModel = 'Ready Player One';
   $scope.character = mainCharacter;
+  $scope.cvfResult = CharacterVersionFactory;
 }]);
